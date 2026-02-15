b5aae0204f937f11d35fe188c87adc6eaacb63dd
Vahid Arzumanov <arzumanovvahid27@gmail.com>
2026-02-15 22:55:14 +0400

    Fix delete_at function to return the modified list instead of None for out-of-range index Add main script to demonstrate delete_at functionality

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -11,6 +11,6 @@ def delete_at(my_list=[], idx=0):
         The modified list or None if the index is out of range.
     """
     if idx < 0 or idx >= len(my_list):
-        return None
+        return my_list
     del my_list[idx]
     return my_list
