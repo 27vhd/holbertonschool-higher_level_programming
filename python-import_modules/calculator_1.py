aae738cd8962add4fa8403c9be8502247beb6680
Vahid Arzumanov <arzumanovvahid27@gmail.com>
2026-02-15 20:51:19 +0400

    Add calculator module and calculation script for basic arithmetic operations

---
+++ python-import_modules/calculator_1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    def add(a, b):
+        """Return the addition of a and b."""
+        return a + b
+
+    def sub(a, b):
+        """Return the subtraction of a and b."""
+        return a - b
+
+    def mul(a, b):
+        """Return the multiplication of a and b."""
+        return a * b
+
+    def div(a, b):
+        """Return the division of a and b."""
+        return a / b
