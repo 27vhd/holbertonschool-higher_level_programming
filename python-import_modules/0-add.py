4c95871c88a5f27319da1f0028f8e72330275e96
Vahid Arzumanov <arzumanovvahid27@gmail.com>
2026-02-15 16:49:01 +0400

    Refactor addition function call in 0-add.py for improved readability

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,6 +2,7 @@
 import add_0
 
 if __name__ == "__main__":
+    add = __import__('add_0').add
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add_0.add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
