8503950d782165f799f9f17b7cf3daece3c1d719
Vahid Arzumanov <arzumanovvahid27@gmail.com>
2026-02-22 21:34:27 +0400

    created

---
+++ python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def safe_print_integer_err(value):
+    """Prints an integer with "{:d}".format().
+
+    Args:
+        value: The value to be printed.
+
+    Returns:
+        True if the value has been correctly printed (it is an integer),
+        False otherwise.
+    """
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError) as e:
+        print("Exception: {}".format(e))
+        return False
