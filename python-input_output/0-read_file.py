6cfcf1043007e9137c7e0fed3ff21999673aac8d
Vahid Arzumanov <arzumanovvahid27@gmail.com>
2026-03-15 23:25:52 +0400

    Add docstrings to module and function

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Module for reading a file and printing it to stdout."""
+
+
 def read_file(filename=""):
+    """Read a text file (UTF8) and print it to stdout."""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
